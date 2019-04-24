6a9f07a98c0bb3fdfdc459470bcedd062c8733f4
Traian <traianmihali696@gmail.com>
2019-04-25 00:13:00 +0300

    add a flag to check if the list requires further sorting

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,8 +1,14 @@
 function bubbleSort(array) {
-  for (let i = 0; i < array.length - 1; i++)
+  for (let i = 0; i < array.length - 1; i++) {
+    let modified = false;
     for (let j = 0; j < array.length - (i + 1); j++)
-      if (array[j] > array[j + 1])
+      if (array[j] > array[j + 1]) {
         [array[j], array[j + 1]] = [array[j + 1], array[j]];
+        modified = true;
+      }
+
+    if (!modified) break;
+  }
 
   return array;
 }
