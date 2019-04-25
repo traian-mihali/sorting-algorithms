1c4c202b9f62fc6849e4f9611dcfc7a3847b5224
Traian <traianmihali696@gmail.com>
2019-04-25 03:14:39 +0300

    add mergeSort implementation

---
+++ mergeSort.js
@@ -0,0 +1,19 @@
+function mergeSort(arr) {
+  if (arr.length < 2) return arr;
+
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  const result = [];
+
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) result.push(left.shift());
+    else result.push(right.shift());
+  }
+  return [...result, ...left, ...right];
+}
